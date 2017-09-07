308fcacd2ba21a567b993de7033733b29f3d98cb
Pasi Junttila <pasi.junttila@live.com>
2017-09-07 17:19:20 +0300

    New project with Spring Boot Initializr.

---
+++ src/main/java/tennisodds/TennisoddsBeApplication.java
@@ -0,0 +1,12 @@
+package tennisodds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisoddsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisoddsBeApplication.class, args);
+	}
+}
